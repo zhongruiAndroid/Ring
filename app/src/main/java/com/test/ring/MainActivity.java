9e211ee4c84f3aa4f4828a542b084f21aed855a6
2380253499@qq.com <2380253499@qq.com>
2018-05-23 18:14:31 +0800

    项目文件

--- app/src/main/java/com/test/ring/MainActivity.java
+++ app/src/main/java/com/test/ring/MainActivity.java
@@ -1,13 +1,52 @@
 package com.test.ring;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.widget.SeekBar;
+
+import com.github.ring.CircleProgress;
 
 public class MainActivity extends AppCompatActivity {
-
+    CircleProgress circleprogress;
+    AppCompatSeekBar seekbar,seekbar2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        circleprogress = (CircleProgress) findViewById(R.id.circleprogress);
+        seekbar = (AppCompatSeekBar) findViewById(R.id.seekbar);
+
+        seekbar.setMax(circleprogress.getMaxProgress());
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                circleprogress.setProgress(progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        seekbar2 = (AppCompatSeekBar) findViewById(R.id.seekbar2);
+        seekbar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
     }
 }
